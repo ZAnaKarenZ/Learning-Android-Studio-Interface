e146c7424624da4b3eb23d272b53f0bf40bb4009
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-10-14 13:58:32 -0600

    Commit in progress

--- app/src/main/java/com/example/entrega1/PiedraPapelTijera.kt
+++ app/src/main/java/com/example/entrega1/PiedraPapelTijera.kt
@@ -1,31 +1,20 @@
 package com.example.entrega1
 
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Button
-import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
-
-
+class PiedraPapelTijera : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_piedra_papel_tijera)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val button = findViewById<Button>(R.id.Button)
-        button.setOnClickListener{
-            val intent = Intent(this, activity_one::class.java)
-            startActivity(intent)
         }
     }
 }
