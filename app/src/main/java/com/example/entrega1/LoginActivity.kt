9ab4189caa833025ae140bb817561daee88e38ae
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-10-22 13:13:02 -0600

    Final Extra Details

--- app/src/main/java/com/example/entrega1/LoginActivity.kt
+++ app/src/main/java/com/example/entrega1/LoginActivity.kt
@@ -1,7 +1,8 @@
 package com.example.entrega1
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
 import android.widget.Button
 import android.content.Intent
 import androidx.activity.enableEdgeToEdge
@@ -23,9 +24,17 @@ class LoginActivity : AppCompatActivity() {
         }
 
         val button = findViewById<Button>(R.id.Button)
-        button.setOnClickListener{
-            val intent = Intent(this, activity_one::class.java)
-            startActivity(intent)
+
+        button.setOnClickListener {
+            val usuario = findViewById<EditText>(R.id.usuario).text.toString()
+            val contraseña = findViewById<EditText>(R.id.contraseña).text.toString()
+
+            if (usuario == "Usuario" && contraseña  == "Contrasea") {
+                val intent = Intent(this, activity_one::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Error.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
