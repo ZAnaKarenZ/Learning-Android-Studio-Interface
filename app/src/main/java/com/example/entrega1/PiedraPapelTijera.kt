c4f8aff751b2871fc45aa9670ab92708eb1a9bf5
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-10-21 19:52:55 -0600

    More details

--- app/src/main/java/com/example/entrega1/PiedraPapelTijera.kt
+++ app/src/main/java/com/example/entrega1/PiedraPapelTijera.kt
@@ -1,6 +1,8 @@
 package com.example.entrega1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +19,10 @@ class PiedraPapelTijera : AppCompatActivity() {
             insets
         }
     }
+    val button1 = findViewById<Button>(R.id.back)
+    button1.setOnClickListener{
+        val intent = Intent(this, PiedraPapelTijera::class.java)
+        startActivity(intent)
+    }
+
 }
