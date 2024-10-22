881b7cc9c9fb4290260a7d512d1bd1a937ecaf56
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-10-21 23:01:59 -0600

    Almost done

--- app/src/main/java/com/example/entrega1/PiedraPapelTijera.kt
+++ app/src/main/java/com/example/entrega1/PiedraPapelTijera.kt
@@ -2,7 +2,11 @@ package com.example.entrega1
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
+import android.widget.TextView
+import android.widget.ImageButton
+import android.graphics.Color
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,12 +23,130 @@ class PiedraPapelTijera : AppCompatActivity() {
             insets
         }
 
+        //Return to previous page
         val button = findViewById<Button>(R.id.back)
         button.setOnClickListener{
             val intent = Intent(this, activity_one::class.java)
             startActivity(intent)
         }
 
-    }
+        //Variables
+        val jugador = findViewById<TextView>(R.id.jugador)
+        val computadora = findViewById<TextView>(R.id.computadora)
 
+        var computadoraScore = 0
+        var jugadorScore = 0
+
+        val buttonPiedra = findViewById<ImageButton>(R.id.piedra)
+        val buttonPapel = findViewById<ImageButton>(R.id.papel)
+        val buttonTijeras = findViewById<ImageButton>(R.id.tijeras)
+
+        //Game
+        buttonPiedra.setOnClickListener{
+
+            //Change color
+            buttonPiedra.setColorFilter(Color.argb(128, 128, 0, 128))
+
+            //Opciones
+            val opciones = listOf("piedra", "papel", "tijeras")
+            val randomChoice = opciones.random()
+            when (randomChoice) {
+                "piedra" -> {
+                    computadoraScore += 1
+                    jugadorScore += 1
+                }
+                "papel" -> {
+                    computadoraScore += 1
+
+                    //Change color
+                    buttonPapel.setColorFilter(Color.argb(128, 128, 0, 128))
+                }
+                "tijeras" -> {
+                    jugadorScore += 1
+
+                    //Change color
+                    buttonTijeras.setColorFilter(Color.argb(128, 128, 0, 128))
+                }
+            }
+
+            //Reset colors
+            Handler().postDelayed({
+                buttonPiedra.clearColorFilter()
+                buttonPapel.clearColorFilter()
+                buttonTijeras.clearColorFilter()
+            }, 1000)
+
+            jugador.text = "Jugador: $jugadorScore"
+            computadora.text = "Computadora: $computadoraScore"
+        }
+
+
+        buttonPapel.setOnClickListener{
+            //Change color
+            buttonPapel.setColorFilter(Color.argb(128, 128, 0, 128))
+
+            val opciones = listOf("piedra", "papel", "tijeras")
+            val randomChoice = opciones.random()
+            when (randomChoice) {
+                "papel" -> {
+                    computadoraScore += 1
+                    jugadorScore += 1
+                }
+                "tijeras" -> {
+                    computadoraScore += 1
+                    //Change color
+                    buttonTijeras.setColorFilter(Color.argb(128, 128, 0, 128))
+                }
+                "piedra" -> {
+                    jugadorScore += 1
+                    //Change color
+                    buttonPiedra.setColorFilter(Color.argb(128, 128, 0, 128))
+                }
+            }
+            //Reset colors
+            Handler().postDelayed({
+                buttonPiedra.clearColorFilter()
+                buttonPapel.clearColorFilter()
+                buttonTijeras.clearColorFilter()
+            }, 1000)
+
+            jugador.text = "Jugador: $jugadorScore"
+            computadora.text = "Computadora: $computadoraScore"
+        }
+
+
+        buttonTijeras.setOnClickListener{
+            //Change color
+            buttonTijeras.setColorFilter(Color.argb(128, 128, 0, 128))
+
+            val opciones = listOf("piedra", "papel", "tijeras")
+            val randomChoice = opciones.random()
+            when (randomChoice) {
+                "tijeras" -> {
+                    computadoraScore += 1
+                    jugadorScore += 1
+                }
+                "piedra" -> {
+                    computadoraScore += 1
+                    //Change color
+                    buttonPiedra.setColorFilter(Color.argb(128, 128, 0, 128))
+                }
+                "papel" -> {
+                    jugadorScore += 1
+                    //Change color
+                    buttonPapel.setColorFilter(Color.argb(128, 128, 0, 128))
+                }
+            }
+
+            //Reset colors
+            Handler().postDelayed({
+                buttonPiedra.clearColorFilter()
+                buttonPapel.clearColorFilter()
+                buttonTijeras.clearColorFilter()
+            }, 1000)
+
+            jugador.text = "Jugador: $jugadorScore"
+            computadora.text = "Computadora: $computadoraScore"
+        }
+    }
 }
